6db790f87f296ffe29c39f5841b59453d9ff7088
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2021-04-29 19:26:05 +0200

    Create Colis.cs

---
+++ Suivi de colis/Colis.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suivi_de_colis
+{
+    class Colis
+    {
+            string id;
+            float longueur;
+            float hauteur;
+            float largeur;
+            bool fragilite
+            public Camion(string id ,float longueur ,float hauteur, float largeur, bool fragilite)
+            {
+                this.id = id;
+                this.longueur = longueur;
+                this.hauteur = hauteur;
+                this.largeur = largeur;
+                this.fragilite = fragilite;
+            }
+
+            public string ID
+            {
+                get
+                {
+                    return id;
+                }
+
+                set
+                {
+                    id = value;
+                }
+            }
+            
+            public float Longueur
+            {
+                get
+                {
+                    return longueur;
+                }
+
+                set
+                {
+                    longueur = value;
+                }
+            }
+            public float Hauteur
+            {
+                get
+                {
+                    return hauteur;
+                }
+
+                set
+                {
+                    hauteur = value;
+                }
+            }
+            public float Largeur
+            {
+                get
+                {
+                    return largeur;
+                }
+
+                set
+                {
+                    largeur = value;
+                }
+            }
+            public float Fragilite
+            {
+                get
+                {
+                    return fragilite;
+                }
+
+                set
+                {
+                    fragilite = value;
+                }
+            }
+
+        
+    }
+}
