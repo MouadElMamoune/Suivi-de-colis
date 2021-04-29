46cd4f3af02f7ba33f07473e2a7c5a53b1d1dbd0
Mouad753 <mmamoune2013@gmail.com>
2021-04-29 14:35:18 +0000

    Interface de planification de livraison et début de l'interface de chargement de colis

---
+++ Suivi de colis/ColisChargement.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Suivi_de_colis
+{
+    partial class ColisChargement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lieuChargementCCcomboBox = new System.Windows.Forms.ComboBox();
+            this.lieuChargementPLlabel = new System.Windows.Forms.Label();
+            this.camionPLlabel = new System.Windows.Forms.Label();
+            this.camionCCcomboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lieuChargementCCcomboBox
+            // 
+            this.lieuChargementCCcomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.lieuChargementCCcomboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lieuChargementCCcomboBox.FormattingEnabled = true;
+            this.lieuChargementCCcomboBox.Location = new System.Drawing.Point(304, 75);
+            this.lieuChargementCCcomboBox.Name = "lieuChargementCCcomboBox";
+            this.lieuChargementCCcomboBox.Size = new System.Drawing.Size(180, 33);
+            this.lieuChargementCCcomboBox.TabIndex = 7;
+            // 
+            // lieuChargementPLlabel
+            // 
+            this.lieuChargementPLlabel.AutoSize = true;
+            this.lieuChargementPLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lieuChargementPLlabel.Location = new System.Drawing.Point(32, 78);
+            this.lieuChargementPLlabel.Name = "lieuChargementPLlabel";
+            this.lieuChargementPLlabel.Size = new System.Drawing.Size(202, 25);
+            this.lieuChargementPLlabel.TabIndex = 6;
+            this.lieuChargementPLlabel.Text = "Lieu de chargement";
+            // 
+            // camionPLlabel
+            // 
+            this.camionPLlabel.AutoSize = true;
+            this.camionPLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camionPLlabel.Location = new System.Drawing.Point(32, 38);
+            this.camionPLlabel.Name = "camionPLlabel";
+            this.camionPLlabel.Size = new System.Drawing.Size(85, 25);
+            this.camionPLlabel.TabIndex = 5;
+            this.camionPLlabel.Text = "Camion";
+            // 
+            // camionCCcomboBox
+            // 
+            this.camionCCcomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.camionCCcomboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camionCCcomboBox.FormattingEnabled = true;
+            this.camionCCcomboBox.Location = new System.Drawing.Point(304, 35);
+            this.camionCCcomboBox.Name = "camionCCcomboBox";
+            this.camionCCcomboBox.Size = new System.Drawing.Size(180, 33);
+            this.camionCCcomboBox.TabIndex = 4;
+            // 
+            // ColisChargement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lieuChargementCCcomboBox);
+            this.Controls.Add(this.lieuChargementPLlabel);
+            this.Controls.Add(this.camionPLlabel);
+            this.Controls.Add(this.camionCCcomboBox);
+            this.Name = "ColisChargement";
+            this.Text = "ColisChargement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox lieuChargementCCcomboBox;
+        private System.Windows.Forms.Label lieuChargementPLlabel;
+        private System.Windows.Forms.Label camionPLlabel;
+        private System.Windows.Forms.ComboBox camionCCcomboBox;
+    }
+}
---
+++ Suivi de colis/PlanificationLivraison.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace Suivi_de_colis
+{
+    partial class PlanificationLivraison
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.camionPLcomboBox = new System.Windows.Forms.ComboBox();
+            this.camionPLlabel = new System.Windows.Forms.Label();
+            this.lieuChargementPLlabel = new System.Windows.Forms.Label();
+            this.lieuChargementPLcomboBox = new System.Windows.Forms.ComboBox();
+            this.colisPLlabel = new System.Windows.Forms.Label();
+            this.nbDestinationPLlabel = new System.Windows.Forms.Label();
+            this.nbDestinationPLtextBox = new System.Windows.Forms.TextBox();
+            this.colisPLbutton = new System.Windows.Forms.Button();
+            this.destinationPLbutton = new System.Windows.Forms.Button();
+            this.destinationPLlabel = new System.Windows.Forms.Label();
+            this.validerPLbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // camionPLcomboBox
+            // 
+            this.camionPLcomboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camionPLcomboBox.FormattingEnabled = true;
+            this.camionPLcomboBox.Location = new System.Drawing.Point(322, 32);
+            this.camionPLcomboBox.Name = "camionPLcomboBox";
+            this.camionPLcomboBox.Size = new System.Drawing.Size(180, 33);
+            this.camionPLcomboBox.TabIndex = 0;
+            // 
+            // camionPLlabel
+            // 
+            this.camionPLlabel.AutoSize = true;
+            this.camionPLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camionPLlabel.Location = new System.Drawing.Point(50, 35);
+            this.camionPLlabel.Name = "camionPLlabel";
+            this.camionPLlabel.Size = new System.Drawing.Size(85, 25);
+            this.camionPLlabel.TabIndex = 1;
+            this.camionPLlabel.Text = "Camion";
+            // 
+            // lieuChargementPLlabel
+            // 
+            this.lieuChargementPLlabel.AutoSize = true;
+            this.lieuChargementPLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lieuChargementPLlabel.Location = new System.Drawing.Point(50, 75);
+            this.lieuChargementPLlabel.Name = "lieuChargementPLlabel";
+            this.lieuChargementPLlabel.Size = new System.Drawing.Size(202, 25);
+            this.lieuChargementPLlabel.TabIndex = 2;
+            this.lieuChargementPLlabel.Text = "Lieu de chargement";
+            // 
+            // lieuChargementPLcomboBox
+            // 
+            this.lieuChargementPLcomboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lieuChargementPLcomboBox.FormattingEnabled = true;
+            this.lieuChargementPLcomboBox.Location = new System.Drawing.Point(322, 72);
+            this.lieuChargementPLcomboBox.Name = "lieuChargementPLcomboBox";
+            this.lieuChargementPLcomboBox.Size = new System.Drawing.Size(180, 33);
+            this.lieuChargementPLcomboBox.TabIndex = 3;
+            // 
+            // colisPLlabel
+            // 
+            this.colisPLlabel.AutoSize = true;
+            this.colisPLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colisPLlabel.Location = new System.Drawing.Point(50, 123);
+            this.colisPLlabel.Name = "colisPLlabel";
+            this.colisPLlabel.Size = new System.Drawing.Size(60, 25);
+            this.colisPLlabel.TabIndex = 4;
+            this.colisPLlabel.Text = "Colis";
+            // 
+            // nbDestinationPLlabel
+            // 
+            this.nbDestinationPLlabel.AutoSize = true;
+            this.nbDestinationPLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nbDestinationPLlabel.Location = new System.Drawing.Point(50, 166);
+            this.nbDestinationPLlabel.Name = "nbDestinationPLlabel";
+            this.nbDestinationPLlabel.Size = new System.Drawing.Size(239, 25);
+            this.nbDestinationPLlabel.TabIndex = 5;
+            this.nbDestinationPLlabel.Text = "Nombre de destinations";
+            // 
+            // nbDestinationPLtextBox
+            // 
+            this.nbDestinationPLtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nbDestinationPLtextBox.Location = new System.Drawing.Point(322, 166);
+            this.nbDestinationPLtextBox.Name = "nbDestinationPLtextBox";
+            this.nbDestinationPLtextBox.Size = new System.Drawing.Size(180, 31);
+            this.nbDestinationPLtextBox.TabIndex = 6;
+            // 
+            // colisPLbutton
+            // 
+            this.colisPLbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colisPLbutton.Location = new System.Drawing.Point(322, 117);
+            this.colisPLbutton.Name = "colisPLbutton";
+            this.colisPLbutton.Size = new System.Drawing.Size(180, 36);
+            this.colisPLbutton.TabIndex = 7;
+            this.colisPLbutton.Text = "Charger";
+            this.colisPLbutton.UseVisualStyleBackColor = true;
+            this.colisPLbutton.Click += new System.EventHandler(this.ColisPLbutton_Click);
+            // 
+            // destinationPLbutton
+            // 
+            this.destinationPLbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.destinationPLbutton.Location = new System.Drawing.Point(322, 215);
+            this.destinationPLbutton.Name = "destinationPLbutton";
+            this.destinationPLbutton.Size = new System.Drawing.Size(180, 36);
+            this.destinationPLbutton.TabIndex = 9;
+            this.destinationPLbutton.Text = "Définir";
+            this.destinationPLbutton.UseVisualStyleBackColor = true;
+            // 
+            // destinationPLlabel
+            // 
+            this.destinationPLlabel.AutoSize = true;
+            this.destinationPLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.destinationPLlabel.Location = new System.Drawing.Point(50, 221);
+            this.destinationPLlabel.Name = "destinationPLlabel";
+            this.destinationPLlabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.destinationPLlabel.Size = new System.Drawing.Size(145, 25);
+            this.destinationPLlabel.TabIndex = 8;
+            this.destinationPLlabel.Text = "Destination(s)";
+            // 
+            // validerPLbutton
+            // 
+            this.validerPLbutton.BackColor = System.Drawing.SystemColors.Desktop;
+            this.validerPLbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.validerPLbutton.Location = new System.Drawing.Point(169, 294);
+            this.validerPLbutton.Name = "validerPLbutton";
+            this.validerPLbutton.Size = new System.Drawing.Size(180, 36);
+            this.validerPLbutton.TabIndex = 10;
+            this.validerPLbutton.Text = "Valider";
+            this.validerPLbutton.UseVisualStyleBackColor = false;
+            // 
+            // PlanificationLivraison
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.validerPLbutton);
+            this.Controls.Add(this.destinationPLbutton);
+            this.Controls.Add(this.destinationPLlabel);
+            this.Controls.Add(this.colisPLbutton);
+            this.Controls.Add(this.nbDestinationPLtextBox);
+            this.Controls.Add(this.nbDestinationPLlabel);
+            this.Controls.Add(this.colisPLlabel);
+            this.Controls.Add(this.lieuChargementPLcomboBox);
+            this.Controls.Add(this.lieuChargementPLlabel);
+            this.Controls.Add(this.camionPLlabel);
+            this.Controls.Add(this.camionPLcomboBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "PlanificationLivraison";
+            this.Text = "Planification de livraison";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox camionPLcomboBox;
+        private System.Windows.Forms.Label camionPLlabel;
+        private System.Windows.Forms.Label lieuChargementPLlabel;
+        private System.Windows.Forms.ComboBox lieuChargementPLcomboBox;
+        private System.Windows.Forms.Label colisPLlabel;
+        private System.Windows.Forms.Label nbDestinationPLlabel;
+        private System.Windows.Forms.TextBox nbDestinationPLtextBox;
+        private System.Windows.Forms.Button colisPLbutton;
+        private System.Windows.Forms.Button destinationPLbutton;
+        private System.Windows.Forms.Label destinationPLlabel;
+        private System.Windows.Forms.Button validerPLbutton;
+    }
+}
