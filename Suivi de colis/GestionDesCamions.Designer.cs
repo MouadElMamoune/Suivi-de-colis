aeb5c03516c2ba14966789319f2ae04282c82069
imanemalawi <64901086+imanemalawi@users.noreply.github.com>
2021-05-28 19:08:51 +0200

    Update GestionDesCamions.Designer.cs

--- Suivi de colis/GestionDesCamions.Designer.cs
+++ Suivi de colis/GestionDesCamions.Designer.cs
@@ -3,6 +3,8 @@ namespace Suivi_de_colis
 {
     partial class GestionDesCamions
     {
+        //private const bool V = true;
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -86,73 +88,7 @@ namespace Suivi_de_colis
             this.AjouterGestionDesChauffeursbutton.TabIndex = 27;
             this.AjouterGestionDesChauffeursbutton.Text = "Ajouter";
             this.AjouterGestionDesChauffeursbutton.UseVisualStyleBackColor = true;
-            /* 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(43, 236);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(280, 31);
-            this.label6.TabIndex = 26;
-            this.label6.Text = "Consommation (L/km)";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(43, 193);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(82, 31);
-            this.label5.TabIndex = 24;
-            this.label5.Text = "Poids";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(43, 146);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(102, 31);
-            this.label4.TabIndex = 23;
-            this.label4.Text = "Modele";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(43, 101);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 31);
-            this.label3.TabIndex = 22;
-            this.label3.Text = "Marque";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(43, 56);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 31);
-            this.label2.TabIndex = 21;
-            this.label2.Text = "Matricule";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(43, 12);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 31);
-            this.label1.TabIndex = 20;
-            this.label1.Text = "ID";
-            *// 
+             
             // PoidsGestionDesCamionstextBox
             // 
             this.PoidsGestionDesCamionstextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -196,55 +132,10 @@ namespace Suivi_de_colis
             this.IDGestionDesCamionstextBox.Margin = new System.Windows.Forms.Padding(4);
             this.IDGestionDesCamionstextBox.Name = "IDGestionDesCamionstextBox";
             this.IDGestionDesCamionstextBox.Size = new System.Drawing.Size(269, 37);
-            this.IDGestionDesCamionstextBox.TabIndex = 15;
-            /* 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(43, 416);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(386, 31);
-            this.label7.TabIndex = 37;
-            this.label7.Text = "Poids maximal de sa cargaison";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(43, 371);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(378, 31);
-            this.label8.TabIndex = 36;
-            this.label8.Text = "Largeur de sa caisse en mètre";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(43, 326);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(382, 31);
-            this.label9.TabIndex = 35;
-            this.label9.Text = "Hauteur de sa caisse en mètre";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(43, 282);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(399, 31);
-            this.label10.TabIndex = 34;
-            this.label10.Text = "Longueur de sa caisse en mètre";
-            *// 
-            // PoidsMaxtextBox
-            // 
+            const int V1 = 15;
+            this.IDGestionDesCamionstextBox.TabIndex = V1;
+            
+            
             this.PoidsMaxtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PoidsMaxtextBox.Location = new System.Drawing.Point(459, 414);
             this.PoidsMaxtextBox.Margin = new System.Windows.Forms.Padding(4);
