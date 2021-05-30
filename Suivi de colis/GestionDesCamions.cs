3ea19a0879ea016f9e8042c28d2b1c2c51412666
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2021-05-30 22:18:09 +0200

    Update GestionDesCamions.cs

--- Suivi de colis/GestionDesCamions.cs
+++ Suivi de colis/GestionDesCamions.cs
@@ -17,5 +17,49 @@ namespace Suivi_de_colis
             InitializeComponent();
         }
         
+        private void AjouterGestionDesChauffeursbutton_Click(object sender, EventArgs e)
+        {   
+            if (IDGestionDesCamionstextBox.Text != "") 
+            {
+                CamionDAO CDAO = new CamionDAO();
+                Camion C;
+                string id = IDGestionDesCamionstextBox.Text;
+                string matricule = MatriculeGestionDesCamionstextBox.Text;
+                string marque = MarqueGestionDesCamionstextBox.Text;
+                string modele = ModeleGestionDesCamionstextBox.Text;
+                float poids = float.Parse(PoidsGestionDesCamionstextBox.Text);
+                float consommation = float.Parse(ConsommationTextBox.Text);
+                float longueur = float.Parse(LongueurtextBox.Text);
+                float hauteur = float.Parse(HauteurtextBox.Text);
+                float largeur = float.Parse(LargeurtextBox.Text);
+                float poids_max = float.Parse(PoidsMaxtextBox.Text);
+                C = new Camion(id, matricule, marque, modele, poids, consommation, longueur, hauteur, largeur, poids_max);
+                CDAO.Ajouter(C);
+            }
+        }
+
+        private void ModifierGestionDesChauffeursbutton_Click(object sender, EventArgs e)
+        {
+            if (IDGestionDesCamionstextBox.Text != "")
+            {
+                CamionDAO CDAO = new CamionDAO();
+                if (CDAO.Selectionner(IDGestionDesCamionstextBox.Text) != null)
+                {
+                    Camion C;
+                    string id = IDGestionDesCamionstextBox.Text;
+                    string matricule = MatriculeGestionDesCamionstextBox.Text;
+                    string marque = MarqueGestionDesCamionstextBox.Text;
+                    string modele = ModeleGestionDesCamionstextBox.Text;
+                    float poids = float.Parse(PoidsGestionDesCamionstextBox.Text);
+                    float consommation = float.Parse(ConsommationTextBox.Text);
+                    float longueur = float.Parse(LongueurtextBox.Text);
+                    float hauteur = float.Parse(HauteurtextBox.Text);
+                    float largeur = float.Parse(LargeurtextBox.Text);
+                    float poids_max = float.Parse(PoidsMaxtextBox.Text);
+                    C = new Camion(id, matricule, marque, modele, poids, consommation, longueur, hauteur, largeur, poids_max);
+                    CDAO.Modifier(C);
+                }
+            }
+        }
     }
 }
