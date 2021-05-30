41c5adb3cbdaf29f5b251f075496426d0b8b6bb8
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2021-05-30 22:55:51 +0200

    Update GestionDesColis.cs

--- Suivi de colis/GestionDesColis.cs
+++ Suivi de colis/GestionDesColis.cs
@@ -16,5 +16,28 @@ namespace Suivi_de_colis
         {
             InitializeComponent();
         }
+        
+        private void AjouterGestionDesColisbutton_Click(object sender, EventArgs e)
+        {   
+            if (IDGestionDesColistextBox.Text != "") 
+            {
+                ColisDAO CDAO = new ColisDAO();
+                Colis C = new Colis(IDGestionDesColistextBox.Text, float.Parse(LongeurGestionDesColistextBox.Text), float.parse(HauteurGestionDesColistextBox.Text), float.parse(LargeurGestionDesColisTxtBox.Text), checkBox1.Checked);
+                CDAO.Ajouter(C);
+            }
+        }
+
+        private void ModifierGestionDesColisbutton_Click(object sender, EventArgs e)
+        {
+            if (IDGestionDesColistextBox.Text != "")
+            {
+                ColisDAO CDAO = new ColisDAO();
+                if (CDAO.Selectionner(IDGestionDesColistextBox.Text) != null)
+                {
+                    Colis C = new Colis(IDGestionDesColistextBox.Text, float.Parse(LongeurGestionDesColistextBox.Text), float.parse(HauteurGestionDesColistextBox.Text), float.parse(LargeurGestionDesColisTxtBox.Text), checkBox1.Checked);
+                    CDAO.Modifier(C);
+                }
+            }
+        }
     }
 }
