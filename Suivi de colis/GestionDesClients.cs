eb02568da48ca7ad71f1d5269f30698ea85238cc
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2021-05-30 22:27:57 +0200

    Update GestionDesClients.cs

--- Suivi de colis/GestionDesClients.cs
+++ Suivi de colis/GestionDesClients.cs
@@ -16,5 +16,27 @@ namespace Suivi_de_colis
         {
             InitializeComponent();
         }
+        private void AjouterGestionDesClientsbutton_Click(object sender, EventArgs e)
+        {   
+            if (IDGestionDesClientstextBox .Text != "") 
+            {
+                ClientDAO CDAO = new ClientDAO();
+                Client C = new Client(IDGestionDesCamionstextBox.Text, NomGestionDesClientstextBox.Text, Convert.ToInt32(nombre_livraisonsGestionDesClientsnumericUpDown.Value));
+                CDAO.Ajouter(C);
+            }
+        }
+
+        private void ModifierGestionDesClientsbutton_Click(object sender, EventArgs e)
+        {
+            if (IDGestionDesClientstextBox .Text != "")
+            {
+                ClientDAO CDAO = new ClientDAO();
+                if (CDAO.Selectionner(IDGestionDesClientstextBox.Text) != null)
+                {
+                    Client C = new Client(IDGestionDesCamionstextBox.Text, NomGestionDesClientstextBox.Text, Convert.ToInt32(nombre_livraisonsGestionDesClientsnumericUpDown.Value));
+                    CDAO.Modifier(C);
+                }
+            }
+        }
     }
 }
