2137f4f372fa64c923cba742862b938a232c7427
mohammedsaiddahchar <65324937+mohammedsaiddahchar@users.noreply.github.com>
2021-05-30 22:52:55 +0200

    Update GestionDesDestinations.cs

--- Suivi de colis/GestionDesDestinations.cs
+++ Suivi de colis/GestionDesDestinations.cs
@@ -16,5 +16,29 @@ namespace Suivi_de_colis
         {
             InitializeComponent();
         }
+        
+        private void AjouterGestionDesColisbutton_Click(object sender, EventArgs e)
+        {   
+            if (IDGestionDesDestinationstextBox.Text != "") 
+            {
+                DestinationDAO DDAO = new DestinationDAO();
+                Destination D = new Destination(IDGestionDesDestinationstextBox.Text, adresse_postaleGestionDesDestinationstextBox.Text, coordonnees_GPSGestionDesDestinationstextBox.Text);
+                DDAO.Ajouter(D);
+            }
+        }
+
+        private void ModifierGestionDesColisbutton_Click(object sender, EventArgs e)
+        {
+            if (IDGestionDesDestinationstextBox.Text != "")
+            {
+                DestinationDAO DDAO = new DestinationDAO();
+                if (CDAO.Selectionner(IDGestionDesDestinationstextBox.Text) != null)
+                {
+                    DestinationDAO DDAO = new DestinationDAO();
+                    Destination D = new Destination(IDGestionDesDestinationstextBox.Text, adresse_postaleGestionDesDestinationstextBox.Text, coordonnees_GPSGestionDesDestinationstextBox.Text);
+                    DDAO.Modifier(D);
+                }
+            }
+        }
     }
 }
